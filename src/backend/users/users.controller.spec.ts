a8e207d3a048659adf3bc7ac67b9d4e5fd9028ff
jyutzio <jyutzio@gmail.com>
2020-01-11 13:22:46 +0100

    Stumped

--- src/backend/users/users.controller.spec.ts
+++ src/backend/users/users.controller.spec.ts
@@ -1,16 +1,39 @@
 /* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["expect", "request"] }] */
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ExecutionContext } from '@nestjs/common';
 import { UsersModule } from './users.module';
+import { UsersService } from './users.service';
+import { AuthGuard } from '../common/guards';
+import { UserDto } from './users.entity';
+
+const mockUser = {
+  id: 1,
+  username: 'Anonymous',
+  provider: 'test',
+  providerId: 'test',
+  dateCreated: new Date(),
+  dateModified: new Date(),
+};
+
+const authGuard = jest.fn().mockImplementation((context: ExecutionContext) => {
+  context.switchToHttp().getRequest().user = mockUser;
+  return true;
+});
 
 describe('User Controller', () => {
   let app: INestApplication;
+  const usersService = { findOne: (): UserDto => mockUser };
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
       imports: [UsersModule],
-    }).compile();
+    })
+      .overrideProvider(UsersService)
+      .useValue(usersService)
+      .overrideGuard(AuthGuard)
+      .useValue(authGuard)
+      .compile();
 
     app = module.createNestApplication();
     await app.init();
@@ -22,10 +45,7 @@ describe('User Controller', () => {
   it('/GET profile', () => {
     return request(app.getHttpServer())
       .get('/profile')
-      .expect({
-        statusCode: 403,
-        error: 'Forbidden',
-        message: 'Forbidden resource',
-      });
+      .expect(200)
+      .expect({ data: mockUser });
   });
 });
